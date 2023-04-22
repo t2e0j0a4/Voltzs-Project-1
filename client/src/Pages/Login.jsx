6e1037ee4b18521eabfb6022e9daf1a88c353ee9
Nagateja <teja91.valiveti@gmail.com>
2023-04-22 12:09:54 +0530

    Comments updated

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -34,18 +34,24 @@ const Login = () => {
 
         <form onSubmit={(e) => {loginFormSubmit(e)}} className='bg-[#ffffff] w-[400px] lg:w-[80%] xl:w-[70%] 2xl:w-[75%] flex flex-col justify-center items-center rounded-[12px] mt-[3rem] gap-[2rem] py-[2rem] px-[1rem] sm:px-[0.6rem] md:px-[1rem]'>
           <h2 className='loginHeadings pageSectionHeadings'>Login</h2>
+
+          {/* Email - Login */}
           <div className="inputGroup loginEmailInputGroup">
             <div data-inputfor="labelemail" className="labelInputDiv">
               <label htmlFor="loginEmail">Email</label>
             </div>
             <input required type="email" name="loginemail" id="loginEmail" className='inputs' value={loginDetails.loginemail} onChange={(e) => {handleLoginInputsChange(e)}} data-inputgiven="labelemail" onFocus={function(e) {handleInputFocus(e)}} onBlur={function(e) {handleInputBlur(e)}}/>
           </div>
+
+          {/* Password - Login */}
           <div className="inputGroup loginPasswordInputGroup">
             <div data-inputfor="labelpassword" className="labelInputDiv">
               <label htmlFor="loginPassword">Password</label>
             </div>
             <input required type="password" name='loginpassword' value={loginDetails.loginpassword} onChange={(e) => {handleLoginInputsChange(e)}} id='loginPassword' className='inputs' data-inputgiven="labelpassword" onFocus={function(e) {handleInputFocus(e)}} onBlur={function(e) {handleInputBlur(e)}}/>
           </div>
+
+          {/* Options - Login */}
           <div className="loginOptions">
             <div className="checkboxOption rememberMeOption">
               <input type="checkbox" name="rememberMe" id="loginRememberMe" value={loginDetails.rememberme} onChange={(e) => {handleLoginInputsChange(e)}}/>
@@ -53,6 +59,7 @@ const Login = () => {
             </div>
             <Link to='/forgotpassword' className="hover:underline">Forgot Password ?</Link>
           </div>
+
           <div className="accountInOptions">
             <button type='submit' className=''>Login</button>
             <button type='button' className="buttonWrapper" onClick={() => {clearInputs()}}>
